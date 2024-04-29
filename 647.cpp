af0413e4e2c5b26681ea20fcafc827de95bc47d2
loukylor <loukylor@gmail.com>
2024-04-29 14:19:12 -0700

    Add problem 647
    time: O(N^2)
    space: O(N)

---
+++ 647.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int count = 0;
+        for (int i = 0; i < s.size(); i++) {
+            count += isPalindrome(s, i, i) + isPalindrome(s, i, i + 1);
+        }
+
+        return count;
+    }
+
+private:
+    int isPalindrome(string& s, int left, int right) {
+        int count = 0;
+        while (left >= 0 && right < s.size() && s[left] == s[right]) {
+            count++;
+            left--;
+            right++;
+        }
+        return count;
+    }
+};
